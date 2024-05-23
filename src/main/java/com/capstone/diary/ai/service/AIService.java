04b30f61031a512f297868bcc96578e73543512b
Ahnheechan <ahc70032@gmail.com>
2024-05-23 16:54:06 +0900

    [fix] Configuration 오류 수정

--- src/main/java/com/capstone/diary/ai/service/AIService.java
+++ src/main/java/com/capstone/diary/ai/service/AIService.java
@@ -7,6 +7,7 @@ import com.capstone.diary.common.RabbitMQConfig;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 
@@ -19,11 +20,18 @@ public class AIService {
 
     private final RabbitTemplate rabbitTemplate;
     private final AIRepository aiRepository;
-    private final RabbitMQConfig config;
+    @Value("${mq.queue-name}")
+    private String QUEUE_NAME;
+
+    @Value("${mq.exchange_name}")
+    private String EXCHANGE_NAME;
+
+    @Value("${mq.routing-key}")
+    private String ROUTING_KEY;
 
     public String sendMessage(AIMessageDto aiMessageDto) {
         AI ai = new AI(aiMessageDto);
-        rabbitTemplate.convertAndSend(config.getEXCHANGE_NAME(),config.getROUTING_KEY(), aiMessageDto);
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME,ROUTING_KEY, aiMessageDto);
         return "메시지 전송 완료";
     }
 
@@ -32,7 +40,7 @@ public class AIService {
         List<AIMessageDto> aiMessageDtos = new ArrayList<>();
         aiRepository.findAll().forEach(o-> aiMessageDtos.add(new AIMessageDto(o)));
         while(true){
-            AIMessageDto message = rabbitTemplate.receiveAndConvert(config.getQUEUE_NAME(), new ParameterizedTypeReference<AIMessageDto>() {});
+            AIMessageDto message = rabbitTemplate.receiveAndConvert(QUEUE_NAME, new ParameterizedTypeReference<AIMessageDto>() {});
             if(message == null){
                 break;
             }
